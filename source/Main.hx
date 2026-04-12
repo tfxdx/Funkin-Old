916a2e17d22277b904e317d96b98e876baeeb907
tfxdx <yunumendozaperez@gmail.com>
2026-04-12 03:21:42 -0500

    fix main android

--- source/Main.hx
+++ source/Main.hx
@@ -9,10 +9,8 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, TitleState));
+		addChild(new FlxGame(1280, 720, TitleState));
 
-		#if !mobile
 		addChild(new FPS(10, 3, 0xFFFFFF));
-		#end
 	}
 }
