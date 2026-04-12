9a4696b0d75f595e438abaff5951d49547279a77
tfxdx <yunumendozaperez@gmail.com>
2026-04-12 16:42:51 -0500

    Update MusicBeatState.hx

--- source/MusicBeatState.hx
+++ source/MusicBeatState.hx
@@ -25,19 +25,7 @@ class MusicBeatState extends FlxUIState
 		return PlayerSettings.player1.controls;
 
 	#if mobile
-	var _hitbox:FlxHitbox;
 	var _virtualpad:FlxVirtualPad;
-
-	public function addHitbox(?keyCount:Int = 3) {
-		_hitbox = new FlxHitbox(keyCount);
-
-		var camMobile = new FlxCamera();
-	    camMobile.bgColor.alpha = 0;
-		FlxG.cameras.add(camMobile, false);
-
-		_hitbox.cameras = [camMobile];
- 		add(_hitbox);
-	}
 
 	public function addVirtualPad(?dpad:FlxDPadMode, ?action:FlxActionMode) {
 		_virtualpad = new FlxVirtualPad(dpad, action);
