19feb68d28b8fb01ee09f89b048bb7970817834b
tfxdx <yunumendozaperez@gmail.com>
2026-04-12 16:22:22 -0500

    Update FlxButton.hx

--- source/mobile/FlxButton.hx
+++ source/mobile/FlxButton.hx
@@ -9,7 +9,9 @@ import flixel.input.IFlxInput;
 import flixel.input.touch.FlxTouch;
 import flixel.math.FlxPoint;
 import flixel.util.FlxDestroyUtil.IFlxDestroyable;
+import flixel.sound.FlxSound;
 
+using StringTools;
 /**
  * A simple button class that calls a function when clicked by the touch.
  */
