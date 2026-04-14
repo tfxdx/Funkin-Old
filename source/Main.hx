83603f2377c7d4651026641fb049981679a0972e
tfxdx <yunumendozaperez@gmail.com>
2026-04-14 16:22:39 -0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -11,6 +11,27 @@ class Main extends Sprite
 		super();
 		addChild(new FlxGame(1280, 720, TitleState));
 
-		addChild(new FPS(0, 0, 0xFFFFFF));
+		addChild(new FPS(10, 3, 0xFFFFFF));
+
+		FlxG.signals.gameResized.add(function (w, h) {
+			if(fpsVar != null)
+				fpsVar.positionFPS(10, 3, Math.min(w / FlxG.width, h / FlxG.height));
+		     if (FlxG.cameras != null) {
+			   for (cam in FlxG.cameras.list) {
+				if (cam != null && cam.filters != null)
+					resetSpriteCache(cam.flashSprite);
+			   }
+			}
+
+			if (FlxG.game != null)
+			resetSpriteCache(FlxG.game);
+		});
+	}
+
+	static function resetSpriteCache(sprite:Sprite):Void {
+		@:privateAccess {
+		        sprite.__cacheBitmap = null;
+			sprite.__cacheBitmapData = null;
+		}
 	}
 }
