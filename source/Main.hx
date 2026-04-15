1feaa73e06232148486c29b28132595e70b280a1
tfxdx <yunumendozaperez@gmail.com>
2026-04-14 19:35:34 -0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -17,22 +17,6 @@ class Main extends Sprite
 		FlxG.signals.gameResized.add(function (w, h) {
 			if(fpsVar != null)
 				fpsVar.positionFPS(10, 3, Math.min(w / FlxG.width, h / FlxG.height));
-		     if (FlxG.cameras != null) {
-			   for (cam in FlxG.cameras.list) {
-				if (cam != null && cam.filters != null)
-					resetSpriteCache(cam.flashSprite);
-			   }
 			}
-
-			if (FlxG.game != null)
-			resetSpriteCache(FlxG.game);
-		});
-	}
-
-	static function resetSpriteCache(sprite:Sprite):Void {
-		@:privateAccess {
-		        sprite.__cacheBitmap = null;
-			sprite.__cacheBitmapData = null;
-		}
 	}
 }
